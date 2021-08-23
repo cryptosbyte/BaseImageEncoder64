0822d795e92a1ff04ebcdbcf035c9584dc2714d9
cryptosbyte <74561974+cryptosbyte@users.noreply.github.com>
2021-08-23 20:13:25 +0100

    🚊📵 RM build/ (Directory)
    Not needed & can be built using TSC (TypeScript Compiler)

--- build/lib/Base64Encode.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Base64Encode = void 0;
-const Base64Encode = (data) => {
-    return Buffer.from(data, "binary").toString("base64");
-};
-exports.Base64Encode = Base64Encode;
